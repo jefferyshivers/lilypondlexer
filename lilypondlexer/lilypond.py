f99f88e7cf75653c103267c17b931b5aa1759809
Jeffery Shivers <jefferyshivers@gmail.com>
2017-03-13 13:14:04 -0400

    WIP - UNSTABLE building the lexer

--- lilypondlexer/lilypond.py
+++ lilypondlexer/lilypond.py
@@ -14,7 +14,8 @@ class LilyPondLexer(RegexLexer):
     aliases = ['lilypond', 'lp']
     filename = ['*.ly', '*.ily']
 
-    builtins = ('set', 'override')
+    builtins = ('once', 'set', 'override', 'paper', 'context', 'layout', 'score',
+                'tempo', 'repeat', 'with')
 
     tokens = {
 
@@ -24,29 +25,32 @@ class LilyPondLexer(RegexLexer):
             # comments
             (r'%\{', Comment.Multiline, 'comment'),
             (r'%.*$', Comment.Single),
+
+            # \functions
+            (r'(\\)(%s)(\s+|\\)' % '|'.join(builtins), Name.Builtin),
             (r'\\[a-zA-Z]*\s*', Name.Function),
 
-            # notes (pitches)
-            (r'[a-g][^a-zA-Z]\s*', Name.Builtin),
-
-            # symbols
-            # TODO
+            # uncap/capitalized symbols
+            (r'[a-zA-Z][a-zA-Z]+\s*', Keyword),
 
             # other non-strings
-            (r'\s*[a-zA-Z]+\s*', Keyword),
+            (r'[a-z][a-z]+', Text),
+
+            # notes (pitches)
+            (r'[a-g]+?', Name.Builtin),
 
             # push scheme mode
             (r'\s*#\(\s*', Punctuation, 'scm-content'),
             # scheme quotes
-            (r'\s*#\'', Punctuation),
+            #(r'\s+#(\'|\`)', Punctuation),
 
             # common notations
-            (r'(\/|\{|\}|\(|\)|\[|\])', Punctuation),
-            (r'(\.|\,|\'|\-|\|)', Punctuation),
-            (r'\s*[a-g]\s*', Keyword),
+            (r'(\#|\/|\{|\}|\(|\)|\[|\])', Punctuation),
+            (r'(\.|\,|\'|\`|\-|\|)', Punctuation),
             (r'\d+', Number.Integer),
+            (r'\d+\.\d+', Number.Float),
             (r'(\=|\:|\:\:)', Operator),
-            (r'(\")(.+?)(\")', String)
+            (r'(\")(.+?)(\")', String.Single)
         ],
 
         'comment': [
