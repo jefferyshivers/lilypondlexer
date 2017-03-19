daf28abf43fecb5edf1fc844d775deb17eac5a2b
Jeffery Shivers <jefferyshivers@gmail.com>
2017-03-18 21:44:17 -0400

    recognize scheme bools

--- lilypondlexer/lilypond.py
+++ lilypondlexer/lilypond.py
@@ -14,8 +14,47 @@ class LilyPondLexer(RegexLexer):
     aliases = ['lilypond', 'lp']
     filename = ['*.ly', '*.ily']
 
-    builtins = ('once', 'set', 'override', 'paper', 'context', 'layout', 'score',
-                'tempo', 'repeat', 'with')
+    builtins = ('once', 'set', 'override', 'paper', 'context', 'layout',
+        'score', 'tempo', 'repeat', 'with')
+
+    grob_interfaces = ('Accidental', 'AccidentalCautionary',
+        'AccidentalPlacement',  'AccidentalSuggestion', 'Ambitus',
+        'AmbitusAccidental', 'AmbitusLine', 'AmbitusNoteHead', 'Arpeggio',
+        'BalloonTextItem', 'BarLine', 'BarNumber', 'BassFigure',
+        'BassFigureAlignment', 'BassFigureAlignmentPositioning',
+        'BassFigureBracket', 'BassFigureContinuation', 'BassFigureLine', 'Beam',
+        'BendAfter', 'BreakAlignGroup', 'BreakAlignment', 'BreathingSign',
+        'ChordName',  'Clef', 'ClefModifier', 'ClusterSpanner',
+        'ClusterSpannerBeacon',  'CombineTextScript', 'CueClef', 'CueEndClef',
+        'Custos', 'DotColumn',  'Dots', 'DoublePercentRepeat',
+        'DoublePercentRepeatCounter', 'DoubleRepeatSlash', 'DynamicLineSpanner',
+        'DynamicText', 'DynamicTextSpanner', 'Episema', 'Fingering',
+        'FingeringColumn', 'Flag', 'FootnoteItem', 'FootnoteSpanner',
+        'FretBoard', 'Glissando', 'GraceSpacing', 'GridLine', 'GridPoint',
+        'Hairpin', 'HorizontalBracket', 'HorizontalBracketText',
+        'InstrumentName', 'InstrumentSwitch', 'KeyCancellation', 'KeySignature',
+        'KievanLigature', 'LaissezVibrerTie', 'LaissezVibrerTieColumn',
+        'LedgerLineSpanner', 'LeftEdge', 'LigatureBracket', 'LyricExtender',
+        'LyricHyphen', 'LyricSpace', 'LyricText', 'MeasureCounter',
+        'MeasureGrouping', 'MelodyItem', 'MensuralLigature', 'MetronomeMark',
+        'MultiMeasureRest', 'MultiMeasureRestNumber', 'MultiMeasureRestText',
+        'NonMusicalPaperColumn', 'NoteCollision', 'NoteColumn', 'NoteHead',
+        'NoteName', 'NoteSpacing', 'OttavaBracket', 'PaperColumn',
+        'ParenthesesItem', 'PercentRepeat', 'PercentRepeatCounter',
+        'PhrasingSlur', 'PianoPedalBracket', 'RehearsalMark', 'RepeatSlash',
+        'RepeatTie', 'RepeatTieColumn', 'Rest', 'RestCollision', 'Script',
+        'ScriptColumn', 'ScriptRow', 'Slur', 'SostenutoPedal',
+        'SostenutoPedalLineSpanner', 'SpacingSpanner', 'SpanBar', 'SpanBarStub',
+        'StaffGrouper', 'StaffSpacing', 'StaffSymbol', 'StanzaNumber', 'Stem',
+        'StemStub', 'StemTremolo', 'StringNumber', 'StrokeFinger',
+        'SustainPedal', 'SustainPedalLineSpanner', 'System', 'SystemStartBar',
+        'SystemStartBrace', 'SystemStartBracket', 'SystemStartSquare',
+        'TabNoteHead', 'TextScript', 'TextSpanner', 'Tie', 'TieColumn',
+        'TimeSignature', 'TrillPitchAccidental', 'TrillPitchGroup',
+        'TrillPitchHead', 'TrillSpanner', 'TupletBracket', 'TupletNumber',
+        'UnaCordaPedal', 'UnaCordaPedalLineSpanner', 'VaticanaLigature',
+        'VerticalAlignment', 'VerticalAxisGroup', 'VoiceFollower',
+        'VoltaBracket', 'VoltaBracketSpanner')
 
     tokens = {
 
@@ -36,13 +75,14 @@ class LilyPondLexer(RegexLexer):
             # other non-strings
             (r'[a-z][a-z]+', Text),
 
+
             # notes (pitches)
             (r'[a-g]+?', Name.Builtin),
 
             # push scheme mode
             (r'\s*#\(\s*', Punctuation, 'scm-content'),
             # scheme quotes
-            #(r'\s+#(\'|\`)', Punctuation),
+            (r'(#)(#)(t|f)', Text),
 
             # common notations
             (r'(\#|\/|\{|\}|\(|\)|\[|\])', Punctuation),
